8029643e6f9ebfb09e758927af401e46bdde870f
xSallus <souza95salomao@gmail.com>
2020-01-28 15:26:40 -0400

    some files modified

---
+++ Android/app/src/main/java/com/xsallus/porc/GameScreen.java
@@ -0,0 +1,14 @@
+package com.xsallus.porc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_screen);
+    }
+}
